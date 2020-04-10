36e156c866cf0a4365101d73b5ebcb47a3657942
Vladimir <luvolunov@gmail.com>
2020-04-10 21:38:13 +0900

    added: touch input

---
+++ src/inputs/touch.input.js
@@ -0,0 +1,29 @@
+export class TouchInput{
+    constructor(el){
+        this.element = el;
+    }
+    onSwipe(cb){
+        const self = this;
+        let downTouch = null,
+            upTouch = null;
+
+        this.element.addEventListener("touchstart", function onStart({touches: [touch]}){
+
+            
+            downTouch = touch;
+
+            self.element.addEventListener("touchmove", onMove);
+            self.element.addEventListener("touchend", onEnd)
+
+            
+            function onMove({touches: [touch]}){
+                upTouch = touch;
+            }
+            function onEnd(){
+                self.element.removeEventListener("touchend", onEnd);
+                self.element.removeEventListener("touchmove", onMove);
+                cb(downTouch, upTouch);
+            }
+        });
+    }
+}
