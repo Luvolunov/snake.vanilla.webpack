a1170ad121bdf52c5223cc7cb4ade63484820c1d
Vladimir <luvolunov@gmail.com>
2020-04-11 23:13:48 +0900

    added: bootstrap with app controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './index.scss'
+import { CanvasController } from './models/canvas.controller'
+import { SnakeController } from './models/snake.controller';
+import { FruitController } from './models/fruit.controller';
+import { AppController } from './models/app.controller';
+
+const cnv = new CanvasController(document.getElementById("canvas"));
+const snake = new SnakeController();
+const fruit = new FruitController(snake);
+
+const app = new AppController(cnv, snake, fruit);
